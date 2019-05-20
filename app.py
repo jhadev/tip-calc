3336cbc7d9dcdb4d297e8c75594d3818ff271b0b
JA <joshappeldev@gmail.com>
2019-05-20 18:42:02 -0400

    added total tax rate

--- app.py
+++ app.py
@@ -29,16 +29,17 @@ def get_tips():
         subtotal += bill_amount
         each_tip = bill_amount * (tip_perc / 100)
         total_tips += each_tip
-        print(f'{person_name} has to tip ${each_tip:.2f}')
+        print(f'\n {person_name} has to tip ${each_tip:.2f}')
 
     tax_total = subtotal * (tax / 100)
     actual_total = tax_total + subtotal + total_tips
+    tip_perc_of_subtotal = (total_tips / subtotal) * 100
 
-    print(f'{divider} \n')
-    print(f'The subtotal is ${subtotal:.2f}')
-    print(f'The tip total is ${total_tips:.2f}')
-    print(f'Total tax amount is ${tax_total:.2f}')
-    print(f'Your total bill including tax is ${actual_total:.2f}')
+    print(f'\n The subtotal is ${subtotal:.2f}')
+    print(
+        f'\n The tip total is ${total_tips:.2f} at a rate of {tip_perc_of_subtotal:.2f}% ')
+    print(f'\n Total tax amount is ${tax_total:.2f}')
+    print(f'\n Your total bill including tax is ${actual_total:.2f}')
 
 
 get_tips()
